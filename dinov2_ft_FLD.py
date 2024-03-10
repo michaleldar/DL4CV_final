a13ab8b3ccf41164f262ef60c5842ed2acddf96d
Michal Eldar <101139814+michaleldar@users.noreply.github.com>
2024-03-10 12:45:24 +0200

    Add files via upload

---
+++ dinov2_ft_FLD.py
@@ -0,0 +1,240 @@
+import torch
+from torchvision import transforms, datasets
+from PIL import Image
+from copy import deepcopy
+from torch import nn, optim
+from torch.utils.data import DataLoader, Dataset, random_split
+import numpy as np
+import pandas as pd
+from sklearn.metrics import roc_auc_score
+import wandb
+import matplotlib.pyplot as plt
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# PATCH_SIZE = 14
+PATCH_SIZE = 8
+
+class ApplyMaskToImage(object):
+    def __init__(self, mask_path):
+        self.mask = Image.open(mask_path).convert('RGB')
+        self.mask = transforms.ToTensor()(self.mask)
+        self.mask = self.mask[0, :, :]
+        self.mask = self.mask.unsqueeze(0)
+        self.mask = self.mask.repeat(3, 1, 1)
+        self.mask = self.mask.to(device)
+        super(ApplyMaskToImage, self).__init__()
+
+    def __call__(self, img):
+        img = transforms.ToTensor()(img)
+        img = img.to(device)
+        img = img * self.mask
+        img = transforms.ToPILImage()(img)
+        return img
+
+
+class DinoVisionTransformerClassifier(nn.Module):
+    def __init__(self):
+        super(DinoVisionTransformerClassifier, self).__init__()
+        # dinov2_vits14 = torch.hub.load("facebookresearch/dinov2", "dinov2_vits14")
+        dinov2_vits16 = torch.hub.load('facebookresearch/dino:main', 'dino_vits16')
+        self.transformer = deepcopy(dinov2_vits16)
+        self.classifier = nn.Sequential(nn.Linear(384, 256), nn.ReLU(), nn.Linear(256, 1))
+
+    def forward(self, x):
+        x = self.transformer(x)
+        x = self.transformer.norm(x)
+        x = self.classifier(x)
+        return x
+
+
+class CustomDataset(Dataset):
+    def __init__(self, csv_file, transform=None, image_path_column="image_path"):
+        self._image_path_column = image_path_column
+        self.df = pd.read_csv(csv_file)
+        self.df = self.df.drop_duplicates(subset=[self._image_path_column])
+        # convert the medical condition to 0/1
+        self.df["medical_condition"] = self.df["medical_condition"].apply(lambda x: 0 if x == "DB92" else 1)
+        self.df = self.df.dropna(subset=["medical_condition", self._image_path_column], how='any')
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        img_name = self.df.iloc[idx][self._image_path_column]
+        image = Image.open(img_name).convert('RGB')
+        label = torch.tensor(float(self.df.iloc[idx]["medical_condition"]))
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+
+def visualize_self_attention(model, image_path):
+    """
+    Visualize the self attention of the model on the given image
+    :param model:
+    :param img:
+    :return:
+    """
+    for p in model.parameters():
+        p.requires_grad = False
+
+    model.eval()
+    img0 = Image.open(image_path).convert('RGB')
+    img = data_transforms['train'](img0)
+    w, h = img.shape[1] - img.shape[1] % PATCH_SIZE, img.shape[2] - img.shape[2] % PATCH_SIZE
+    img = img[:, :w, :h].unsqueeze(0)
+    w_featmap = img.shape[-2] // PATCH_SIZE
+    h_featmap = img.shape[-1] // PATCH_SIZE
+
+    attentions = model.transformer.get_last_selfattention(img)  # model.get_last_selfattention(img)
+    nh = attentions.shape[1]  # number of head
+
+    # we keep only the output patch attention
+    attentions = attentions[0, :, 0, 1:].reshape(nh, -1)
+    print(attentions.shape)
+
+    # we keep only a certain percentage of the mass
+    val, idx = torch.sort(attentions)
+    val /= torch.sum(val, dim=1, keepdim=True)
+    cumval = torch.cumsum(val, dim=1)
+    threshold = 0.6  # We visualize masks obtained by thresholding the self-attention maps to keep xx% of the mass.
+    th_attn = cumval > (1 - threshold)
+    idx2 = torch.argsort(idx)
+    for head in range(nh):
+        th_attn[head] = th_attn[head][idx2[head]]
+
+    th_attn = th_attn.reshape(nh, w_featmap // 2, h_featmap // 2).float()
+
+    # interpolate
+    th_attn = nn.functional.interpolate(th_attn.unsqueeze(0), scale_factor=PATCH_SIZE, mode="nearest")[0].cpu().numpy()
+
+    attentions = attentions.reshape(nh, w_featmap // 2, h_featmap // 2)
+    attentions = nn.functional.interpolate(attentions.unsqueeze(0), scale_factor=PATCH_SIZE, mode="nearest")[
+        0].cpu().numpy()
+    attentions_mean = np.mean(attentions, axis=0)
+    plt.figure(figsize=(6, 6), dpi=200)
+
+    plt.subplot(3, 3, 1)
+    plt.title("Original", size=6)
+    plt.imshow(img0)
+    plt.axis("off")
+
+    plt.subplot(3, 3, 2)
+    plt.title("Attentions Mean", size=6)
+    plt.imshow(attentions_mean)
+    plt.axis("off")
+
+    for i in range(6):
+        plt.subplot(3, 3, i + 4)
+        plt.title("Attentions " + str(i), size=6)
+        plt.imshow(attentions[i])
+        plt.axis("off")
+    rnd_text = str(np.random.randint(100000))
+    print("random: ", rnd_text)
+    plt.savefig(f"/home/michalel/PycharmProjects/basic/attention_fld_{rnd_text}.png")
+
+data_transforms = {
+        "train": transforms.Compose(
+            [
+                ApplyMaskToImage('/home/michalel/PycharmProjects/basic/US_mask.jpg'),
+                transforms.CenterCrop((720, 1000)),
+                # transforms.Resize((224, 224)),
+                transforms.Resize((480, 480)),
+                # transforms.RandomRotation(360),
+                # transforms.RandomHorizontalFlip(),
+                # transforms.RandomVerticalFlip(),
+                transforms.ToTensor()
+            ]
+        ),
+    }
+
+if __name__ == '__main__':
+    # dinov2_vits14 = torch.hub.load("facebookresearch/dinov2", "dinov2_vits14")
+    # dinov2_vits16 = torch.hub.load("facebookresearch/dinov2", "dinov2_vits16")
+
+    wandb.init(project="pred_FLD_w_dino")
+
+    csv_path = '/home/michalel/PycharmProjects/basic/us_full_dataset.csv'
+    data = CustomDataset(csv_file=csv_path, transform=data_transforms['train'])
+    train_size = int(0.8 * len(data))
+    val_size = len(data) - train_size
+    # fix the seed for reproducibility
+    generator1 = torch.Generator().manual_seed(42)
+    train_dataset, val_dataset = random_split(data, [train_size, val_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=0)
+    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=0)
+
+    """# visualize the first image in the dataset, and print its shape
+    img, label = data[0]
+    print(img.shape)
+    plt.imshow(img.permute(1, 2, 0))
+    plt.show()"""
+
+
+    model = DinoVisionTransformerClassifier()
+    model = model.to(device)
+    model = model.train()
+    wandb.watch(model, log_freq=100)
+
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-6)
+    num_epochs = 15
+
+    epoch_losses = []
+    for epoch in range(num_epochs):
+        print("Epoch: ", epoch)
+
+        batch_losses = []
+        for data in train_loader:
+            # get the input batch and the labels
+            batch_of_images, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # model prediction
+            output = model(batch_of_images.to(device)).squeeze(dim=1)
+
+            # compute loss and do gradient descent
+            loss = criterion(output, labels.float().to(device))
+            loss.backward()
+            optimizer.step()
+
+            batch_losses.append(loss.item())
+
+        epoch_losses.append(np.mean(batch_losses))
+        print(f"Mean epoch loss: {epoch_losses[-1]}")
+        # evaluate the model on the validation set
+        model = model.eval()
+        predictions = []
+        y_true = []
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                predictions.extend(outputs.squeeze().tolist())
+                y_true.extend(labels.tolist())
+            predictions = np.array(predictions)
+            y_true = np.array(y_true)
+            val_loss = criterion(torch.tensor(predictions).to(device), torch.tensor(y_true).float().to(device))
+        print(f"Validation loss: {val_loss.item()}")
+        accuracy = np.mean(np.round(predictions) == y_true)
+        roc_auc = roc_auc_score(y_true, predictions)
+        print(f"Validation accuracy: {accuracy}", f"ROC AUC: {roc_auc}")
+        wandb.log({"epoch_loss": epoch_losses[-1],
+                   "validation_loss": val_loss.item(),
+                   "accuracy": accuracy, "roc_auc": roc_auc})
+
+    # save the trained model
+    torch.save(model.state_dict(), "/home/michalel/PycharmProjects/basic/dino_ft_FLD.pth")
+    print("Finished training!")
+    wandb.log({"scatter": wandb.plot.scatter(
+        x=np.array(y_true), y=np.array(predictions),
+        title="true (x) vs. predicted (y)",
+    )})
+    image_path = "/net/mraid20/export/genie/LabData/Data/10K/aws_lab_files/ultrasound/jpg/1002254441/00_00_visit/20220915/092714.jpg"
+    img = Image.open(image_path).convert('RGB')
+    img = data_transforms['train'](img)
+    # visualize_self_attention(model, img)
