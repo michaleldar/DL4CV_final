0daee4bb497fa917106487d216be0333b64593c8
michalel <michal.eldar1@mail.huji.ac.il>
2024-03-10 16:28:29 +0200

    back to normal

--- CNN_FLD.py
+++ CNN_FLD.py
@@ -110,12 +110,11 @@ def main(dataset_path='/home/michalel/PycharmProjects/basic/us_full_dataset.csv'
     model = models.resnet50(pretrained=True)
     # print(model.fc.in_features)
     # remove the last layer
-    model = nn.Sequential(*list(model.children())[:-3])
+    # model = nn.Sequential(*list(model.children())[:-1])
     # print the width of the last layer
     # print(model.fc.in_features)
     model.fc = nn.Sequential(
-        nn.Linear(4096, 512),
-        # nn.Linear(model.fc.in_features, 512),
+        nn.Linear(model.fc.in_features, 512),
         nn.ReLU(),
         nn.Linear(512, 256),
         nn.ReLU(),
