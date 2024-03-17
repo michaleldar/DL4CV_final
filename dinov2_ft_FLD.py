d92ef817fa96669f62cbd6632ed7aabfcc8337fc
michalel <michal.eldar1@mail.huji.ac.il>
2024-03-17 18:07:26 +0200

    fsd

--- dinov2_ft_FLD.py
+++ dinov2_ft_FLD.py
@@ -273,7 +273,12 @@ def main(dataset_path='/home/michalel/PycharmProjects/basic/us_full_dataset.csv'
                 # Note: You might need to adjust the label tensor shape depending on your loss function's expectation
                 # If your loss function expects labels to have the same batch size as outputs,
                 # you might need to expand or repeat the labels tensor.
+                print(labels.shape)
+                print(labels.expand_as(outputs).shape)
+                print(outputs.shape)
                 loss = criterion(outputs, labels.expand_as(outputs).squeeze())
+
+
 
                 # Backward pass: Compute gradient of the loss with respect to model parameters
                 loss.backward()
