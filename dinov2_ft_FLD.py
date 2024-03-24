d7bd72d5fb1f9d9686bfec42ba49af654f77b388
michalel <michal.eldar1@mail.huji.ac.il>
2024-03-24 11:19:25 +0200

    Gfd

--- dinov2_ft_FLD.py
+++ dinov2_ft_FLD.py
@@ -11,6 +11,7 @@ import wandb
 import matplotlib.pyplot as plt
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("device: ", device)
 PATCH_SIZE = 8
 
 class ApplyMaskToImage(object):
