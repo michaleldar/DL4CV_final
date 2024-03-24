bbaff0e9b41e27a35a34bf4e7fc183de2d6370ac
michalel <michal.eldar1@mail.huji.ac.il>
2024-03-24 11:45:57 +0200

    debug CNN

--- CNN_FLD.py
+++ CNN_FLD.py
@@ -44,7 +44,7 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, idx):
         print("idx: ", idx)
-        if idx in self.df.index.tolist():
+        if idx in self.indexes:
             print("#1 Data leakage detected!")
             sys.exit(1)
         self.indexes.append(idx)
